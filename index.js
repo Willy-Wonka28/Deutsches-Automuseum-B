cc4fb1c3786f7f3fb9ec6958fcaaae7fff40c8d6
Mr Banana <willy28@batman.com>
2024-03-19 16:57:50 +0100

    updates

---
+++ index.js
@@ -0,0 +1,28 @@
+function hideMaterials() {
+    var materialContainer = document.getElementById("menu_list");
+    var materials = materialContainer.querySelectorAll(".materials");
+
+    // Hide materials
+    for (var i = 0; i < materials.length; i++) {
+        materials[i].style.visibility = "hidden";
+        materials[i].style.fontSize = "0";
+        materials[i].style.padding = "0";
+    }
+}
+
+function showMaterials() {
+    var materialContainer = document.getElementById("menu_list");
+    var materials = materialContainer.querySelectorAll(".materials");
+
+    // Show materials
+    for (var i = 0; i < materials.length; i++) {
+        materials[i].style.visibility = "visible";
+        materials[i].style.fontSize = "15px";
+        materials[i].style.padding = "20px";
+    }
+
+    setTimeout(function () {
+        hideMaterials();
+    }, 5000);
+}
+
